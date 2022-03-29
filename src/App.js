2a72620e0b42dcf91999b657d2889ab0279ecd68
zablackafro <zahidabdikadir12@gmail.com>
2022-03-29 18:23:45 -0500

    created a listing form

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
-import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Category from './pages/Category';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import CreateListing from './pages/CreateListing';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>

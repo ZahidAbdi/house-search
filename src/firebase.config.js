d4b92c3702a494539ee2bae013d4ba4c7e8c9e9d
zablackafro <zahidabdikadir12@gmail.com>
2022-03-29 19:17:42 -0500

    enabled the geolocation api and also added functionality to the onSubmit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyAlSVJcoene_t8UMc4kwKMd2rwkjeQPHCU',
-	authDomain: 'housing-search-app.firebaseapp.com',
-	projectId: 'housing-search-app',
-	storageBucket: 'housing-search-app.appspot.com',
-	messagingSenderId: '25510455871',
-	appId: '1:25510455871:web:6004d58d6127ae7276668a',
-}
+  apiKey: 'AIzaSyAlSVJcoene_t8UMc4kwKMd2rwkjeQPHCU',
+  authDomain: 'housing-search-app.firebaseapp.com',
+  projectId: 'housing-search-app',
+  storageBucket: 'housing-search-app.appspot.com',
+  messagingSenderId: '25510455871',
+  appId: '1:25510455871:web:6004d58d6127ae7276668a',
+};
 
 // Initialize Firebase
-initializeApp(firebaseConfig)
-export const db = getFirestore()
+initializeApp(firebaseConfig);
+export const db = getFirestore();
